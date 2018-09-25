19c2aba88b03503320471fb5a6a441c1cde73331
Alonso Montero <31372753+AlonsoM45@users.noreply.github.com>
2018-09-25 01:06:25 -0600

    RSA encryption

--- Log In/Solucion Computacional/LogIn/app/src/main/java/com/example/ruben/login/User.java
+++ Log In/Solucion Computacional/LogIn/app/src/main/java/com/example/ruben/login/User.java
@@ -1,11 +1,15 @@
 package com.example.ruben.login;
 
+import java.math.BigInteger;
 import java.util.Calendar;
 import java.util.Date;
 
 public class User {
     private static String email = "fakemailalonso@gmail.com";
     private static String password = "fakepassword";
+    private static int publicKeyOne;
+    private static BigInteger publicKeyTwo;
+    private static int privateKey;
     private static int failures = 0;
     private static Calendar lastAttempt = null;
 
@@ -55,5 +59,24 @@ public class User {
         }
 
         return failures >= 3;
+    }
+
+    public static void encrypt(String message){
+        int P = 7; //randomPrime();
+        int Q = 11; //randomPrime();
+        int N = P * Q;
+        publicKeyOne = N;
+        BigInteger M = new BigInteger(message.getBytes());
+        int E = 40; //random(2, P * Q) que no divida a N
+        BigInteger C = M.modPow(BigInteger.valueOf(E), BigInteger.valueOf(N));
+        publicKeyTwo = C;
+        int D = (4 * (P - 1) * (Q - 1) + 1)/ E;
+        privateKey = D;
+    }
+
+    public static String decrypt(){
+        BigInteger M = publicKeyTwo.modPow(BigInteger.valueOf(privateKey),
+                BigInteger.valueOf(publicKeyOne));
+        return M.toByteArray().toString();
     }
 }
